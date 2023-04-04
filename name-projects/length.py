c8d11185ad32c09766561283f9225b393512f5e1
varma-cephas <112039818+varma-cephas@users.noreply.github.com>
2023-04-04 08:36:32 +0000

    added-name-projects

---
+++ name-projects/length.py
@@ -0,0 +1,3 @@
+fullName = input("Enter full name: ")
+nameLength = len(fullName)
+print(f"The length of your name is {nameLength}")
---
+++ name-projects/middleName.py
@@ -0,0 +1,6 @@
+fullName = input("Enter full name: ")
+getNames = fullName.split(" ")
+getMiddleName = getNames[1]
+print(f"Your middle name is {getMiddleName}")
+
+
